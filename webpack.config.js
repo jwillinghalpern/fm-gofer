d2f6d7df46d34a5d585960153ad73fd3572db02b
Joshua Willing <jwillinghalpern@gmail.com>
2021-03-25 17:17:32 -0700

    change build to output commonjs and umd files.

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,47 @@
 const path = require('path');
+const distPath = path.resolve(__dirname, 'dist');
+const mode = 'production';
 
-module.exports = {
-  mode: 'production',
-  entry: path.resolve(__dirname, 'src/index.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    // filename: 'index_bundle.js',
-    // libraryExport: 'default',
-    library: {
-      name: 'FMPromise',
-      type: 'umd',
-      export: 'FMPromise',
-      umdNamedDefine: true,
+const moduleRules = {
+  rules: [
+    {
+      test: /\.(js)$/,
+      exclude: /node_modules/,
+      use: 'babel-loader',
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-    ],
-  },
+  ],
 };
+
+const entry = path.resolve(__dirname, 'src/index.js');
+
+module.exports = [
+  {
+    output: {
+      path: distPath,
+      filename: 'main-umd.js',
+      library: {
+        name: 'FMPromise',
+        type: 'umd',
+        export: 'FMPromise',
+        umdNamedDefine: true,
+      },
+    },
+    module: moduleRules,
+    entry: entry,
+    mode: mode,
+  },
+  {
+    output: {
+      path: distPath,
+      filename: 'main-cjs.js',
+      library: {
+        name: 'FMPromise',
+        type: 'commonjs',
+        export: 'FMPromise',
+      },
+    },
+    module: moduleRules,
+    entry: entry,
+    mode: mode,
+  },
+];
