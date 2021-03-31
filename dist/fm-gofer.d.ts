89dd6edae753b390aea1cd047bfa5dc830ebe4d3
Joshua Willing <jwillinghalpern@gmail.com>
2021-03-31 00:51:08 -0700

    add fm-obj-timeout build

--- dist/fm-gofer.d.ts
+++ dist/fm-gofer.d.ts
@@ -3,11 +3,18 @@
  */
 declare class FM {
     /**
+     * Call to resolve a saved callback promise.
+     * @param id - callback id
+     * @param [resolveOrReject = 'resolve'] - 'resolve' or 'reject'
+     * @param [data = null] - any data you wish to return to the webapp. NOTE, FM passes all function params as text, so if you return JSON, be sure to JSON.parse() it.
+     */
+    static FMGofer#runCallback(id: number, resolveOrReject?: string, data?: string): void;
+    /**
      * Perform a FileMaker Script with option. FM can return a result by resolving or rejecting
      * @param script - name of script
      * @param [data = null] - data you wish to send to fm. It will be nested in the `data` property of the script parameter
      * @param [option = 0] - FM script option between 0 and 5
-     * @param [timeout = 1000] - timeout in ms. 0 will wait indefinitely.
+     * @param [timeout = 3000] - timeout in ms. 0 will wait indefinitely.
      * @param [timeoutMessage = 'The FM script call timed out'] - custom message if the call times out.
      * @returns a promise that FileMaker can resolve or reject
      */
@@ -16,7 +23,7 @@ declare class FM {
      * Perform a FileMaker Script. FM can return a result by resolving or rejecting
      * @param script - name of script
      * @param data - you wish to send to fm. It will be nested in the `data` property of the script parameter
-     * @param [timeout = 1000] - timeout in ms. 0 will wait indefinitely.
+     * @param [timeout = 3000] - timeout in ms. 0 will wait indefinitely.
      * @param [timeoutMessage = 'The FM script call timed out'] - custom message if the call times out.
      * @returns a promise that FileMaker can resolve or reject
      */
