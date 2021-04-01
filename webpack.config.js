f1b1a76a135bd8dd5879702a89a22cb8a629c4d5
Joshua Willing <jwillinghalpern@gmail.com>
2021-04-01 16:23:23 -0700

    export named functions instead of class

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = [
       library: {
         name: 'FMGofer',
         type: 'umd',
-        export: 'default',
+        // export: 'default',
         // umdNamedDefine: true,
       },
     },
