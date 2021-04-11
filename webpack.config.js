c11f12063d199d2166c723266803260f7c443c9e
Joshua Willing <jwillinghalpern@gmail.com>
2021-04-11 01:47:46 -0700

    add ie11 build target

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,30 @@ module.exports = [
     mode: mode,
   },
   {
+    // same as the umd above, but target es5 and polyfill promise for ie11
+    entry: ['core-js/stable/promise', path.resolve(__dirname, 'src/index.js')],
+    target: 'es5',
+    output: {
+      globalObject: 'this',
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'fm-gofer.ie11.js',
+      library: {
+        name: 'FMGofer',
+        type: 'umd',
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+      ],
+    },
+    mode: mode,
+  },
+  {
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
       globalObject: 'this',
